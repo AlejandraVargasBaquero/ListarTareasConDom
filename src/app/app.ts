19d42423e54652c031b881cf05e4a401a301cc7e
Alejandra Vargas Baquero <alejandravargasbaquero801@gmail.com>
2026-08-28 20:12:22 -0500

    Calculadora

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,48 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularJorgeDesarrolloDeApps');
+  num1: number = 0;
+  num2: number = 0;
+  operador: string = '+';
+  resultado: number = 0;
+  historial: string[] = [];
+
+  operar() {
+    switch (this.operador) {
+      case '+':
+        this.resultado = this.num1 + this.num2;
+        break;
+      case '-':
+        this.resultado = this.num1 - this.num2;
+        break;
+      case '*':
+        this.resultado = this.num1 * this.num2;
+        break;
+      case '/':
+        this.resultado = this.num2 !== 0 ? this.num1 / this.num2 : NaN;
+        break;
+      case '%':
+        this.resultado = this.num1 % this.num2;
+        break;
+      default:
+        this.resultado = 0;
+    }
+
+    this.historial.unshift(
+      `${this.num1} ${this.operador} ${this.num2} = ${this.resultado}`
+    );
+  }
+
+  limpiarHistorial() {
+    this.historial = [];
+  }
 }
